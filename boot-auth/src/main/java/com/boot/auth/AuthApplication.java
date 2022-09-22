815570852f50a4fb9849369c1a872e1ce963efff
haeyonghahn <yong80211@gmail.com>
2022-09-22 22:04:30 +0900

    fix: 모듈 변경

--- boot-auth/src/main/java/com/boot/auth/AuthApplication.java
+++ boot-auth/src/main/java/com/boot/auth/AuthApplication.java
@@ -1,4 +1,4 @@
-package com.boot.user;
+package com.boot.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserServiceApplication {
+public class AuthApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
--- data-user/src/main/java/com/data/user/User.java
+++
@@ -1,4 +0,0 @@
-package com.data.user;
-
-public class User {
-}
