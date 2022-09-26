aa9142c0a39a1427083b8d0cfd47a4fb1fe0cafd
haeyonghahn <yong80211@gmail.com>
2022-09-26 19:34:01 +0900

    feat: add endpoint

--- boot-first-service/src/main/java/com/boot/service/FirstServiceApplication.java
+++ boot-first-service/src/main/java/com/boot/service/FirstServiceApplication.java
@@ -1,7 +1,10 @@
 package com.boot.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @SpringBootApplication
 public class FirstServiceApplication {
@@ -9,4 +12,6 @@ public class FirstServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FirstServiceApplication.class, args);
     }
+
+
 }
